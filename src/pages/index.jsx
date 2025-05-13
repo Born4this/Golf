dc382bd5eaa7973488bb06457cc67f64b85e5f7a
Michael <mchaelmorris@gmail.com>
2025-05-13 01:35:39 -0400

    index

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // If you want everyone to land on /auth:
+    router.replace('/auth');
+
+    // Or, if you’d prefer to send logged-in users straight to league-selector:
+    // const token = typeof window !== 'undefined' && localStorage.getItem('token');
+    // router.replace(token ? '/league-selector' : '/auth');
+  }, [router]);
+
+  return null;
+}
