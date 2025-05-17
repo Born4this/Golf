9674fcf02fc39f3185b43102f947070352edeb36
Michael <mchaelmorris@gmail.com>
2025-05-17 04:41:44 -0400

    stest3

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -64,7 +64,7 @@ export default function Auth() {
           <img
             src="images/titlelogo.jpg"
             alt="Fantasy Fairway"
-            className="h-42 w-auto"
+            className="h-32 w-auto"
           />
         </div>
 
