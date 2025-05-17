7b34d51418ddc37c3807becbb7c5412fb5e97774
Michael <mchaelmorris@gmail.com>
2025-05-17 04:46:23 -0400

    stest4

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -60,11 +60,11 @@ export default function Auth() {
       {/* card */}
       <div className="max-w-md mx-auto mt-12 p-8 bg-white shadow-lg rounded-2xl">
         {/* Logo in place of the heading */}
-        <div className="flex justify-center mb-4">
+        <div className="flex justify-center mb-3">
           <img
             src="images/titlelogo.jpg"
             alt="Fantasy Fairway"
-            className="h-32 w-auto"
+            className="h-28 w-auto"
           />
         </div>
 
